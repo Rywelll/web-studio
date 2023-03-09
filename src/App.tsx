ea68370b5ab3b7463efed17df1aa1727da462b54
Vladislav Rybalko <coldjasper@gmail.com>
2023-03-09 19:38:16 +0600

    Создание проекта

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return null
+  return (
+      <div>
+        <h1>Hello world</h1>
+      </div>
+  )
 }
 
 export default App;
